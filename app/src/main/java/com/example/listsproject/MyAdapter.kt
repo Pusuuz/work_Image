c5199f2c2dbf7e14698afc3b0acea9414d5e3810
HP <madaminochilov08@gmail.com>
2022-10-26 19:46:57 +0500

    KALFAFHLAHF

--- app/src/main/java/com/example/listsproject/MyAdapter.kt
+++ app/src/main/java/com/example/listsproject/MyAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.listsproject
 
+import android.content.res.ColorStateList
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat.getColor
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 
@@ -26,12 +30,41 @@ class MyAdapter : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
     override fun onBindViewHolder(viewHolder: MyViewHolder, position: Int) {
 
-       val myImage = viewHolder.itemView.findViewById<ImageView>(R.id.imageView)
+        val myImage = viewHolder.itemView.findViewById<ImageView>(R.id.imageView)
+        val myText = viewHolder.itemView.findViewById<TextView>(R.id.namethird)
+        val myLayout = viewHolder.itemView.rootView
+
+
+        val contextt = viewHolder.itemView.context
+        val stringArray = contextt.resources.getStringArray(R.array.programming_laguages)
+
+        val textRes = stringArray[position]
+        myText.text=textRes
+
+
+
+        val resColor = when(position){
+            0->R.color.one
+            1-> R.color.two
+            2-> R.color.three
+            3-> R.color.four
+            4-> R.color.five
+            5-> R.color.six
+            else -> R.color.six
+        }
+        val color = ColorStateList.valueOf(getColor(contextt,resColor))
+        myLayout.backgroundTintList = color
+
+
 
         val imgRes = when(position){
+            0->R.drawable.js
             1-> R.drawable.c_plus
             2-> R.drawable.kotlin
-            else -> R.drawable.js
+            3-> R.drawable.swift
+            4-> R.drawable.roby
+            5-> R.drawable.dart
+            else -> R.drawable.dart
         }
 
 
@@ -39,6 +72,6 @@ class MyAdapter : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
     }
 
-    override fun getItemCount() = 10
+    override fun getItemCount() = 6
 
 }
